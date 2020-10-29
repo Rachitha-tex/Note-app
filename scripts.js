dcf82a6dee3cb35cad0e6f55847fe61f6b26c2b6
Rachitha-tex <56627425+Rachitha-tex@users.noreply.github.com>
2020-10-29 08:58:51 +0530

    using tutorials

---
+++ scripts.js
@@ -0,0 +1,63 @@
+const addBtn=document.querySelector(".add");
+const notes=JSON.parse(localStorage
+    .getItem("notes"));
+
+    if(notes){
+        notes.forEach(note=>{
+            addNewnote(note);
+        })
+    }
+
+addBtn.addEventListener('click',()=>{
+    addNewnote();
+})
+
+function addNewnote(text=''){
+   //to add in the window shoul be inside div
+   const note=document.createElement('div'); 
+   note.classList.add('note');
+
+   note.innerHTML=`
+                <div class="notes">
+                 <div class="tools">
+                 <button class="edit"><i class="fas fa-edit"></i></button>
+                 <button class="delete"><i class="fas fa-trash"></i></button>
+                 </div>
+                  <div class="main ${text ?'':'hidden'} "></div>
+                  <textarea  class=" ${text ?'hidden':''}"></textarea>
+                  </div>
+   `;
+const editBtn=note.querySelector(".edit")
+const deleteBtn=note.querySelector(".delete")
+const main=note.querySelector(".main")
+const textArea=note.querySelector("textarea");
+
+textArea.value=text;
+main.innerHTML=marked(text);
+
+editBtn.addEventListener('click',()=>{
+    main.classList.toggle('hidden');
+    textArea.classList.toggle('hidden');
+})
+deleteBtn.addEventListener('click',()=>{
+    note.remove();
+})
+
+textArea.addEventListener('input',(e)=>{
+    const {value}=e.target;
+    main.innerHTML=marked(value);
+
+    updateLocal();
+})
+
+document.body.appendChild(note);
+}
+
+function updateLocal(){
+    const textNote=document.querySelectorAll('textarea');
+    const notes=[];
+    textNote.forEach(function(note){
+        notes.push(note.value);
+    })
+    localStorage.setItem('notes',JSON.stringify(notes));
+}
